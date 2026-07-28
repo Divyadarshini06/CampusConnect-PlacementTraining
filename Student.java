5fcae07eedd77f9105c6e5d28eb6cd3a4a501d70
Divya Darshini V <divyadharshiniv06@gmail.com>
2026-07-28 16:25:26 +0530

    Initial commit

---
+++ Student.java
@@ -0,0 +1,26 @@
+/**
+ * Student
+ */
+public class Student {
+
+    public void setName(String string) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setName'");
+    }
+
+    public void setAge(int i) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setAge'");
+    }
+
+    public String getName() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getName'");
+    }
+
+    public String getAge() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAge'");
+    }
+
+}
