3a838be5b178528043ee6d0a2d007ef93bf9eef7
Divya Darshini V <divyadharshiniv06@gmail.com>
2026-07-29 21:37:44 +0530

    Initial commit

--- QueueUseStack.java
+++ QueueUseStack.java
@@ -1,6 +1,6 @@
 import java.util.Stack;
 
-public class StackUseQueue {
+public class QueueUseStack{
     public static class queue {
         Stack<Integer> s1 = new Stack<>();
         Stack<Integer> s2 = new Stack<>();
